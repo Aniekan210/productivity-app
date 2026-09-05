4806047b230f156536ddf204fa15b4c643cd6b89
Aniekan210 <ani8dar@gmail.com>
2026-09-05 17:07:54 -0300

    wecsd

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { emailOTPClient } from "better-auth/client/plugins";
 
 const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BETTER_AUTH_URL,
   plugins: [emailOTPClient()],
 });
 
